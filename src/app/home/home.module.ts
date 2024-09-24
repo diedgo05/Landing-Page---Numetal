7cfb2d5fb0acb7c80543022d12c5d73287042e2d
diedgo05 <233358@ids.upchiapas.edu.mx>
2024-09-23 20:52:49 -0600

    add: adding title, content and the principal image

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
 
 
 
 @NgModule({
   declarations: [
   
-    HomeComponent
+    HomeComponent,
+    MainComponent
   ],
   imports: [
     CommonModule
